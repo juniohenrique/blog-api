d62f4d3ff460a1f93b0f2f46da59a89e3b515f8a
Fernando Papito <fernando@qaninja.io>
2018-02-26 21:22:22 -0300

    bug fix no deploy.sh e nova versão

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ echo "Deploy no ambiente: $1"
 rm -rf .git
 git init
 heroku git:remote -a blog-api-$1
-heroku config:set RAKE_ENV=$1
+heroku config:set RACK_ENV=$1
 git add . && git commit -m "new deploy"
 git push heroku master --force
